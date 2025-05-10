4ec68697fea05c76dbe32bc100fe1a3032e4d0f0
Martin <martin.allenstein@gmail.com>
2025-05-11 00:07:46 +0700

    Add Terrain, Modify Scripts
    Add
    -Overwold Terrain
    
    Modify
    -Player Controls Script

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] private int speed;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer playerSprite;
 
     private Rigidbody rb;
     private Vector3 movement;
 
+    //Animation Parameters
+    private const string IS_WALK_PARAM = "IsWalk";
+    private const string IS_JUMP_PARAM = "IsJump";
+    
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,6 +25,19 @@ public class PlayerControls : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         
         movement = new Vector3(x, 0, z).normalized;
+        
+        animator.SetBool(IS_WALK_PARAM, movement != Vector3.zero);
+
+        //Flip Player
+        if (x != 0 && x != 0)
+        {
+            playerSprite.flipX = true;
+        }
+
+        if (x != 0 && x > 0)
+        {
+            playerSprite.flipX = false;
+        }
     }
 
     private void FixedUpdate()
